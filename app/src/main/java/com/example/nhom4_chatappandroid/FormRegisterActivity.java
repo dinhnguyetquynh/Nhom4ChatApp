28bc3b2c3db5ab68fada2b6205ccf61c2b706477
dinhnguyetquynh <dquynhd202@gmail.com>
2024-04-03 00:33:56 +0700

    dangky.xml

---
+++ app/src/main/java/com/example/nhom4_chatappandroid/FormRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.nhom4_chatappandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_register);
+    }
+}
