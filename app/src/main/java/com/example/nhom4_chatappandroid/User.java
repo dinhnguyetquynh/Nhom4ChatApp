38262073ed5a910bd0d797e49aaaa69d35a82c1f
dinhnguyetquynh <dquynhd202@gmail.com>
2024-04-02 20:44:37 +0700

    daxoafoldergiaodien

--- app/src/main/java/com/example/nhom4_chatappandroid/User.java
+++ app/src/main/java/com/example/nhom4_chatappandroid/User.java
@@ -1,6 +1,56 @@
 package com.example.nhom4_chatappandroid;
 
+import java.time.LocalDateTime;
+
 public class User {
     private int image;
+    private String nameUser;
+    private String message;
+    private LocalDateTime time;
+
+    public User(int image, String nameUser, String message, LocalDateTime time) {
+        this.image = image;
+        this.nameUser = nameUser;
+        this.message = message;
+        this.time = time;
+    }
+
+    public User() {
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    
+
 
 }
